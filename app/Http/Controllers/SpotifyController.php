a29c69f2e76ff1f18a731c490d2b2832f2c7bd84
ItsukiKoido <af22080@shibaura-it.ac.jp>
2024-06-11 23:12:17 +0900

    spotify_uri

--- app/Http/Controllers/SpotifyController.php
+++ app/Http/Controllers/SpotifyController.php
@@ -22,7 +22,7 @@ class SpotifyController extends Controller
             'Authorization'=> 'Basic ' . $base64,
         ])->asForm()->post('https://accounts.spotify.com/api/token', [
             'code' => $code,
-            'redirect_uri' => 'https://live-app-998bc1d15ef2.herokuapp.com//spotify-test',
+            'redirect_uri' => 'https://c17039964ea14ad1a4cc905104872779.vfs.cloud9.us-east-1.amazonaws.com/spotify-test',
             'grant_type' => 'authorization_code',
         ]);
         
@@ -39,7 +39,7 @@ class SpotifyController extends Controller
         $profileDataJson = $profileData->json();
         
         // プロフィールデータの出力
-        dd($profileDataJson, $accessToken, $access_token);
+        //dd($profileDataJson, $accessToken, $access_token);
         
         session(['access_token' => $access_token, 'profileDataJson' => $profileDataJson]);
         
@@ -159,6 +159,6 @@ class SpotifyController extends Controller
         $live = json_decode($request->input('live'));
         session(['live' => $live]);
         $state = bin2hex(random_bytes(16));
-        return redirect("https://accounts.spotify.com/authorize?client_id=190f5fe85108404c8ad184ae8358cdb5&response_type=code&redirect_uri=https://live-app-998bc1d15ef2.herokuapp.com/spotify-test&state={$state}&scope=playlist-modify-private,playlist-modify-public")->with(["artist_name"]);
+        return redirect("https://accounts.spotify.com/authorize?client_id=190f5fe85108404c8ad184ae8358cdb5&response_type=code&redirect_uri=https://c17039964ea14ad1a4cc905104872779.vfs.cloud9.us-east-1.amazonaws.com/spotify-test&state={$state}&scope=playlist-modify-private,playlist-modify-public")->with(["artist_name"]);
     }
 }
